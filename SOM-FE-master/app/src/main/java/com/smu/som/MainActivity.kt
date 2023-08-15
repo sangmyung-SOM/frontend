6a2a523854b4df2b9f0db19a73fa994081427604
rantris <rantris@gmail.com>
2023-08-15 21:42:10 +0900

    - STOMP 모듈 반영  - 기본 샘플 코드 추가

--- SOM-FE-master/app/src/main/java/com/smu/som/MainActivity.kt
+++ SOM-FE-master/app/src/main/java/com/smu/som/MainActivity.kt
@@ -6,18 +6,76 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.gmail.bishoybasily.stomp.lib.Event
+import com.gmail.bishoybasily.stomp.lib.StompClient
 import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
 import com.kakao.sdk.user.model.AgeRange
+import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.OkHttpClient
+import org.json.JSONObject
 
 
 // 메인 화면 Activity
 class MainActivity : AppCompatActivity() {
 
+    // 안드로이드 애뮬에서 테스트 할때는 localhost 대신 10.0.2.2 사용
+    val url = "ws://10.0.2.2:8080/ws"
+
+    fun runStomp(){
+        val intervalMillis = 1000L
+        val client = OkHttpClient()
+        val stomp = StompClient(client, intervalMillis)
+        stomp.url = url;
+
+        // connect
+        var stompConnection = stomp.connect().subscribe {
+            when (it.type) {
+                Event.Type.OPENED -> {
+                    Log.i(TAG, it.toString())
+                }
+                Event.Type.CLOSED -> {
+                    Log.i(TAG, it.toString())
+                }
+                Event.Type.ERROR -> {
+                    Log.i(TAG, it.toString())
+                }
+            }
+        }
+
+        // subscribe
+        var topic = stomp.join("/topic/public").subscribe {
+            Log.i(TAG, it)
+        }
+
+        /*
+        {
+    "type": "TALK",
+    "sender": "som",
+    "message": "test"
+         */
+
+        val data = JSONObject()
+        data.put("type", "TALK")
+        data.put("sender", "som")
+        data.put("message", "test")
+
+        // send
+        stomp.send("/app/chat.sendMessage", data.toString()).subscribe {
+            if (it) {
+                Log.i(TAG, it.toString())
+            }
+        }
+
+        //topic.dispose()
+        // disconnect
+        //stompConnection.dispose()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // runStomp();
 
         // 카카오톡 사용자 정보 요청 (기본)
         UserApiClient.instance.me { user, error ->
