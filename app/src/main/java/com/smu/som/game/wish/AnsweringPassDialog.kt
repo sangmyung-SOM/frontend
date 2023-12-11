5aa2e547356a14ad0f9782bc05d4cef3b205b9f8
Jung So Young <soyoung5611@gmail.com>
2023-12-12 02:45:57 +0900

    feat: 질문패스 개수 뱃지 알림 구현

--- app/src/main/java/com/smu/som/game/wish/AnsweringPassDialog.kt
+++ app/src/main/java/com/smu/som/game/wish/AnsweringPassDialog.kt
@@ -1,5 +1,6 @@
 package com.smu.som.game.wish
 
+import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.Context
 import android.graphics.Color
@@ -17,19 +18,19 @@ import com.smu.som.game.GameConstant
 import org.json.JSONException
 
 
-class AnsweringPassDialog(context: Context, private val questionList: ArrayList<Question>?, val stomp: StompClient) : Dialog(context) {
+class AnsweringPassDialog(context: Context, private val questionList: ArrayList<Question>?, val stomp: StompClient, private val passCard : Int) : Dialog(context) {
 
     val bundle: Bundle = Bundle()
     var request = JsonObject()
     // 소원권 중 질문패스권이 있는 경우
     // 윷을 던지고 나서 패스를 누르고 말을 놓을 수 있음 (패널티 없는 패스)
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) // 배경 투명하게 만들기
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dialog_quesiton_wish_pass)
         setCanceledOnTouchOutside(false) // 다이얼로그 바깥 부분 눌러도 안닫히게
 
-        // passBtn 왼쪽 위에 패스권 갯수 뱃지로 표시 -- 나중에 구현
     }
 
     fun showPopup() {
@@ -40,7 +41,12 @@ class AnsweringPassDialog(context: Context, private val questionList: ArrayList<
 
         clickPassBtn()
         clickCloseBtn()
+        setPassCard()
+    }
 
+    private fun setPassCard() {
+        val passCardTxt : TextView = findViewById(R.id.passCard)
+        passCardTxt.text = passCard.toString()
     }
 
     private fun clickPassBtn() {
