e6033f0a1a23e562340127f976b4f5a8a16f8202
Jung So Young <soyoung5611@gmail.com>
2024-01-17 00:52:02 +0900

    fix. GameTestActivity false로 변경 및 질문기록 api 호출 주석 처리

--- app/src/main/java/com/smu/som/game/reportQnA/model/service/SaveQnAService.kt
+++ app/src/main/java/com/smu/som/game/reportQnA/model/service/SaveQnAService.kt
@@ -38,50 +38,50 @@ class SaveQnAService() {
             }
         )
 
-        // Post 요청 보내기
-        //post 요청
-        val gson = GsonBuilder()
-            .setLenient()
-            .create()
-
-        // Retrofit을 초기화합니다.
-        val retrofit = Retrofit.Builder()
-            .baseUrl(RetrofitCreator.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .build()
-
-        val gameRoomApi = retrofit.create(AnswerReportAPI::class.java)
-
-        val call = qnaList.question?.let {
-            gameRoomApi.sendQnA(GameConstant.GAMEROOM_ID, qnaList.answer,
-                it, qnaList.playerId)
-        }
-
-        call?.enqueue(object : Callback<Boolean> {
-            override fun onResponse(
-                call: Call<Boolean>,
-                response: Response<Boolean>
-            ) {
-                if (response.isSuccessful) {
-                    Log.d("saveQnAService", "onResponse: $response")
-
-                } else {
-                    Log.d("saveQnAService", "onResponse: $response")
-                }
-                // 404 에러가 발생할 경우 response.errorBody()를 통해 확인
-                try {
-                    val errorBodyString = response.errorBody()?.string()
-                    Log.d("saveQnAService", "Error Body: $errorBodyString")
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
-            }
-
-            override fun onFailure(call: Call<Boolean>, t: Throwable) {
-                Log.d("saveQnAService", "onFailure: $t")
-            }
-        })
-
+//        // Post 요청 보내기
+//        //post 요청
+//        val gson = GsonBuilder()
+//            .setLenient()
+//            .create()
+//
+//        // Retrofit을 초기화합니다.
+//        val retrofit = Retrofit.Builder()
+//            .baseUrl(RetrofitCreator.BASE_URL)
+//            .addConverterFactory(GsonConverterFactory.create(gson))
+//            .build()
+//
+//        val gameRoomApi = retrofit.create(AnswerReportAPI::class.java)
+//
+//        val call = qnaList.question?.let {
+//            gameRoomApi.sendQnA(GameConstant.GAMEROOM_ID, qnaList.answer,
+//                it, qnaList.playerId)
+//        }
+//
+//        call?.enqueue(object : Callback<Boolean> {
+//            override fun onResponse(
+//                call: Call<Boolean>,
+//                response: Response<Boolean>
+//            ) {
+//                if (response.isSuccessful) {
+//                    Log.d("saveQnAService", "onResponse: $response")
+//
+//                } else {
+//                    Log.d("saveQnAService", "onResponse: $response")
+//                }
+//                // 404 에러가 발생할 경우 response.errorBody()를 통해 확인
+//                try {
+//                    val errorBodyString = response.errorBody()?.string()
+//                    Log.d("saveQnAService", "Error Body: $errorBodyString")
+//                } catch (e: IOException) {
+//                    e.printStackTrace()
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<Boolean>, t: Throwable) {
+//                Log.d("saveQnAService", "onFailure: $t")
+//            }
+//        })
+//
 
     }
 
